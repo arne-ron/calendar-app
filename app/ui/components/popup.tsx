b30622f145f2c3b892527b7fb1018fd94a30cfa0
arne-ron <arne.roenneberg@gmail.com>
2025-02-14 21:08:42 +0000

    changed className: string | undefined -> className?: string Learned that otherwise argument is required and needs to be actively set to undefined

--- app/ui/components/popup.tsx
+++ app/ui/components/popup.tsx
@@ -35,7 +35,7 @@ export function Popup(
     }: {
         ref: RefObject<HTMLDialogElement | null>,
         backdrop?: boolean,
-        className: string | undefined,
+        className?: string,
         children?: ReactNode[]
     }
 ) {
