80816f88a49c124a29152a9114e992fee1d30522
arne-ron <arne.roenneberg@gmail.com>
2025-04-08 20:25:48 +0100

    Fixed missing suspense boundary for CRS error in LoginForm

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,6 @@
 import {Metadata} from "next";
 import {LoginForm} from "@/app/ui/forms/login-form";
+import {Suspense} from "react";
 
 
 export const metadata: Metadata = {
@@ -15,7 +16,9 @@ export default function Page() {
 
     return (
         <div className='w-full h-full flex flex-col items-center justify-center'>
-            <LoginForm />
+            <Suspense fallback={<p>Loading...</p>}>
+                <LoginForm />
+            </Suspense>
             <p className='mt-3'>You need to log in to continue</p>
         </div>
     )
