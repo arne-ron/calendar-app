38f27d41cf2047bd646debf069f3bf0ccd1271a8
arne-ron <arne.roenneberg@gmail.com>
2025-04-10 02:06:29 +0100

    changed year display to be bold in year-view.tsx

--- app/ui/timeframes/year-view.tsx
+++ app/ui/timeframes/year-view.tsx
@@ -16,14 +16,14 @@ export function YearView({ events }: { events: Event[]}) {
 
     return (
         <div className='flex flex-col w-full p-4'>
-            <p className='text-xl'>
+            <p className='text-xl font-bold'>
                 2025
             </p>
             <div className='grid grid-cols-4 gap-x-3 gap-y-3 h-full w-full mt-2'>
                 {months.flatMap((month, index) => {
                     return (
                         <Link href={`/calendar?view=month&month=${month}`} key={`month_${month}`} className='bg-gray-50/80 p-2 rounded-lg'>
-                            <p className='font-bold mb-1'>{month}</p>
+                            <p className='mb-1'>{month}</p>
                             <div className='grid grid-cols-7 gap-1'>
                                 {...range(offsets[index]).map((_, i) =>
                                     <div key={`offset_${i}`} />
