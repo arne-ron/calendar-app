0b5fa571d23f431f7b36b613a09997ab6e9d890c
arne-ron <arne.roenneberg@gmail.com>
2025-04-12 22:35:07 +0100

    functional adding and removing the first of first layer

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -10,7 +10,7 @@ import React, {useState} from "react";
 export default function Page() {
     const [data, setData] = useState<data[]>([
             {
-                text: 'Initial Element',
+                text: 'empty',
                 color: 'rgba(187,255,153,0.4)',
                 arr: []
             }
@@ -19,12 +19,25 @@ export default function Page() {
 
     return (
         <div className='flex items-center justify-center h-full'>
-                <Recurser position={[0]} data={data} setData={setData}/>
+            <Recurser position={[0]} data={data} setData={setData}/>
             <button onClick={() => {
                 console.log(JSON.stringify(data));
                 console.log(data)
             }}>
                 Submit
+            </button>
+            <button
+                onClick={() => {
+                    setData((prev) => {
+                        const [first] = prev;
+                        return [{
+                            ...first,
+                            arr: first.arr.filter((_, i) => i !== 0)
+                        }];
+                    })
+                }}
+            >
+                |remove first layer
             </button>
         </div>
     )
--- app/user/recurser.tsx
+++ app/user/recurser.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import {CreateTag} from "@/app/ui/components/create-tag";
+
 
 export interface data {
     text: string,
@@ -18,7 +20,25 @@ export interface RecurserProps {
 
 
 export function Recurser({ position, data, setData }: RecurserProps) {
-    function addElem() {
+    const [simple, setSimple] = useState<boolean>(true)
+
+    // Find the current element to render
+    let arr = data;
+    let element: data = {text: '', color: '', arr: []};
+    for (const pos of position) {
+        element = arr[pos];
+        arr = element.arr;
+    }
+
+
+    React.useEffect(
+        () => setSimple(element.text !== 'and' && element.text !== 'or' && element.text !== 'empty'),
+        [element.text]
+    )
+
+
+
+    function addElem(name: string) {
         let count = 0;
         setData((prevData: data[]) => {
             const newData = [...prevData]; // Shallow copy of root array
@@ -27,7 +47,7 @@ export function Recurser({ position, data, setData }: RecurserProps) {
                 if (count === 0) {
                 newData[0].arr = [
                     ...newData[0].arr,
-                    { text: 'second layer', color: 'rgba(160,153,255,0.4)', arr: [] }
+                    { text: name, color: 'rgba(160,153,255,0.4)', arr: [] }
                 ]
                 }
                 count++;
@@ -45,7 +65,7 @@ export function Recurser({ position, data, setData }: RecurserProps) {
             if (count == 0) {
                 current[lastPos] = {
                     ...current[lastPos],
-                    arr: [...current[lastPos].arr, { text: 'further layer', color: 'rgba(255,153,153,0.4)', arr: [] }]
+                    arr: [...current[lastPos].arr, { text: name, color: 'rgba(255,153,153,0.4)', arr: [] }]
                 };
                 console.log('3 is called');
             }
@@ -57,23 +77,50 @@ export function Recurser({ position, data, setData }: RecurserProps) {
         console.log('is called');
     }
 
-    // Find the current element to render
-    let arr = data;
-    let element;
-    for (const pos of position) {
-        element = arr[pos];
-        arr = element.arr;
-    }
+
 
     return (
-        <div className='flex flex-row gap-3  min-w-16 min-h-10 px-3 py-3 rounded-lg h-min items-center' style={{backgroundColor: element?.color}}>
-            <p>{element?.text}</p>
-            {arr.map((_, i) => (
+        <div className={(element.text === 'empty' && arr.length !== 0) ? 'flex-col' :'flex flex-row gap-3  min-w-16 min-h-10 px-3 py-3 rounded-lg h-min items-center'} style={{backgroundColor: (element.text === 'empty' && arr.length !== 0) ? undefined :  element?.color}}>
+            {element.text === 'empty' && arr.length === 0 &&
+                <CreateTag onSubmit={addElem}/>
+            }
+            {simple &&
+                <button
+                    key='simplbe_button'
+                    className={'flex flex-row gap-1 items-center'}
+                    onClick={() => console.log('remove the simple block (not yet implemented)')}
+                >
+                    <div key='simple_color_swadge' className={`h-3 w-3 rounded-full ml-1`} style={{backgroundColor: element.color}}></div>
+                    <p key='simple_text'>{element.text}</p>
+                </button>
+            }
+            {!simple && element.text !== 'empty' && arr.length === 0 &&
+                // Leading CreateKey
+                <CreateTag key={'leading_createTag'} onSubmit={addElem}/>
+            }
+            {!simple && element.text !== 'empty' && [
+                // Mapping all tags, connected by text
+                arr.flatMap((tag, i: number) => {
+                    console.log("+", JSON.stringify(tag))
+                    return [
+                        i !== 0 &&
+                            <button key={'combiner' + element.text + '_' + i} onClick={() => console.log('remove the simple block (not yet implemented)')}>{element.text + 1 + "_" + i.toString()}</button>,
+                        <Recurser
+                            key={'element' + tag.text + '_' + i}
+                            position={[...position, i]}
+                            data={data}
+                            setData={setData}
+                        />
+                    ]
+                }),
+
+                // always have trailing createTag
+                <button key={'trailing_text'} onClick={() => console.log('remove the complex block (not yet implemented)')}>{element.text + 2}</button>,
+                <CreateTag key={'trailing_createTag'} onSubmit={addElem}/>
+            ]}
+            {simple || element.text === 'empty' && arr.map((_, i) => (
                 <Recurser key={i} position={[...position, i]} data={data} setData={setData} />
             ))}
-            <button className='bg-white rounded-full w-6 h-6' onClick={addElem}>
-                +
-            </button>
         </div>
     );
 }
